63e6c892e51efd69b4cb648ac128b7d0ab811c8a
Daiki Aminaka <1991.daiki@gmail.com>
2014-02-16 22:34:31 +0900

    Comment outs were deleted.

--- DLImages.py
+++ DLImages.py
@@ -2,26 +2,6 @@ from bs4 import BeautifulSoup
 import urllib2, urllib
 import sys, os
 
-"""
-url = sys.argv[1]
-response = urllib2.urlopen(url)
-html = response.read()
-soup = BeautifulSoup(html)
-
-os.mkdir("./"+url.split("/")[2])
-for img in soup.find_all("img"):
-    src = img.get("src")
-    fname = src.split("/")[-1]
-    if "?" in fname:
-        fname = fname.split("?")[0]
-    DLPage = ""
-    DLFile = src
-    if "http" not in src:
-        DLFile = url+DLFile
-    
-    print DLFile
-    urllib.urlretrieve(DLFile, url.split("/")[2]+"/"+fname)
-"""
 
 def main(url):
     response = urllib2.urlopen(url)
