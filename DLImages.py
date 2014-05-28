9824ed413571d1c6dd25945982a5341b1d65a028
Daiki Aminaka <1991.daiki@gmail.com>
2014-05-28 19:29:15 +0900

    this could be used in Python3.x

--- DLImages.py
+++ DLImages.py
@@ -1,4 +1,7 @@
 import os
+import urllib
+from bs4 import BeautifulSoup
+import urllib2
 
 
 SUPPORT_IMAGES = (".jpg",".jpeg",".png",".gif",".tiff",".eps",)
@@ -7,8 +10,6 @@ def rmQ(fname):
     return fname.split("?")[0]
 
 def getImgURL(src, url):
-#    print src
-#    print [src.endswith(support) for support in SUPPORT_IMAGES]
     if True in [src.endswith(support) for support in SUPPORT_IMAGES]:
         if "http" not in src:
             return url+src
@@ -23,11 +24,8 @@ def mkdir(dirName):
     except Exception as e:
         if dirName != "./DLImages":
             print e
-#        pass # if dirName already exist
 
 def DLImages(AllImages, url, dirName):
-    import urllib
-
     successNum = 0
     for img in AllImages:
         src = img.get("src")
@@ -45,18 +43,15 @@ def DLImages(AllImages, url, dirName):
             if fname in os.listdir(dirName):
                 fname = fname + str(successNum)
             urllib.urlretrieve(imgURL, dirName+"/"+fname)
-            print "[ Success ] " + imgURL, fname
+            print("[ Success ] %s %s" % (imgURL, fname))
             successNum += 1
         except Exception as e:
             print e
-            print "[ Failed ] " + imgURL        
+            print("[ Failed ] %s" % imgURL)
 
     return successNum
 
-def main(url, dirName="./DLImages"):
-    from bs4 import BeautifulSoup
-    import urllib2
-    
+def main(url, dirName="./DLImages"):    
     mkdir(dirName)
     response = urllib2.urlopen(url)
     html = response.read()
@@ -65,7 +60,7 @@ def main(url, dirName="./DLImages"):
     imgNum = len(AllImages)
     successNum = DLImages(AllImages, url, dirName)
 
-    print successNum, "images could be downloaded (in", imgNum, "images)."
+    print("%d images could be downloaded (in %d images)." % (successNum, imgNum))
 
 if __name__ == "__main__":
     import sys
@@ -74,4 +69,4 @@ if __name__ == "__main__":
     elif len(sys.argv) == 3:
         main(sys.argv[1], sys.argv[2])
     else:
-        print "Usage: python DLImage.py URL [directory]"
+        print("Usage: python DLImage.py URL [directory]")
